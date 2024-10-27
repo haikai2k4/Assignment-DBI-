5b505b15828884127da63b09b509206c64944365
haikai2k4 <hahuyhoang200@gmail.com>
2024-10-27 17:37:13 +0700

    Update AssignmentDBI.sql

--- AssignmentDBI.sql
+++ AssignmentDBI.sql
@@ -1,4 +1,4 @@
-﻿CREATE DATABASE QLHoSoBenhAn
+CREATE DATABASE QLHoSoBenhAn
 USE QLHoSoBenhAn
 
 
@@ -158,4 +158,86 @@ ALTER TABLE ChiTietPhieuKhamBenh ADD CONSTRAINT FK_BENH_ChiTietBenh FOREIGN KEY 
 ALTER TABLE ToaThuoc ADD CONSTRAINT FK_ChiTietDonThuoc_ToaThuoc FOREIGN KEY (IDDonThuoc) REFERENCES ChiTietDonThuoc(IDDonThuoc)
 ALTER TABLE ChiTietDonThuoc ADD CONSTRAINT FK_Thuoc_ChiTietDonThuoc FOREIGN KEY (IDThuoc) REFERENCES Thuoc(IDThuoc)
 
-
+GO
+INSERT [dbo].[BacSi] ([IDBacSi], [HoVaTenBS], [DiaChiBS], [PHONEBS], [IDKHoa]) VALUES (N'BS1      ', N'Phạm Đăng Bảo', N'Quận 9', N'0981264555', N'K1       ')
+INSERT [dbo].[BacSi] ([IDBacSi], [HoVaTenBS], [DiaChiBS], [PHONEBS], [IDKHoa]) VALUES (N'BS2      ', N'Hà Huy Hoàng', N'Quận 10', N'0982345666', N'K1       ')
+INSERT [dbo].[BacSi] ([IDBacSi], [HoVaTenBS], [DiaChiBS], [PHONEBS], [IDKHoa]) VALUES (N'BS3      ', N'Vũ Ngọc Quang', N'Quận 12', N'0983456777', N'K2       ')
+INSERT [dbo].[BacSi] ([IDBacSi], [HoVaTenBS], [DiaChiBS], [PHONEBS], [IDKHoa]) VALUES (N'BS4      ', N'Trần Phát Đạt', N'Quận 7', N'0984567888', N'K2       ')
+INSERT [dbo].[BacSi] ([IDBacSi], [HoVaTenBS], [DiaChiBS], [PHONEBS], [IDKHoa]) VALUES (N'BS5      ', N'Lương Hồ Tuấn Hậu', N'Quận 1', N'0985678999', N'K3       ')
+GO
+INSERT [dbo].[BENH] ([IDBENH], [TenBenh]) VALUES (N'B1       ', N'Tăng Huyết Áp')
+INSERT [dbo].[BENH] ([IDBENH], [TenBenh]) VALUES (N'B2       ', N'Suy Tim')
+INSERT [dbo].[BENH] ([IDBENH], [TenBenh]) VALUES (N'B3       ', N'Viêm Ruột Thừa')
+INSERT [dbo].[BENH] ([IDBENH], [TenBenh]) VALUES (N'B4       ', N'Gãy Xương')
+INSERT [dbo].[BENH] ([IDBENH], [TenBenh]) VALUES (N'B5       ', N'U Nang Buồng Trứng')
+GO
+INSERT [dbo].[ChiTietDonThuoc] ([IDDonThuoc], [SoLuongThuoc], [IDThuoc]) VALUES (N'DT1      ', 30, N'T1       ')
+INSERT [dbo].[ChiTietDonThuoc] ([IDDonThuoc], [SoLuongThuoc], [IDThuoc]) VALUES (N'DT2      ', 30, N'T2       ')
+INSERT [dbo].[ChiTietDonThuoc] ([IDDonThuoc], [SoLuongThuoc], [IDThuoc]) VALUES (N'DT3      ', 10, N'T3       ')
+INSERT [dbo].[ChiTietDonThuoc] ([IDDonThuoc], [SoLuongThuoc], [IDThuoc]) VALUES (N'DT4      ', 20, N'T4       ')
+INSERT [dbo].[ChiTietDonThuoc] ([IDDonThuoc], [SoLuongThuoc], [IDThuoc]) VALUES (N'DT5      ', 12, N'T5       ')
+GO
+INSERT [dbo].[ChiTietPhieuKhamBenh] ([IDPKB], [TrieuChung], [IDBENH], [IDBacSi]) VALUES (N'PKB1     ', N'Đau Đầu, Chóng Mặt, Buồn Nôn', N'B1       ', N'BS1      ')
+INSERT [dbo].[ChiTietPhieuKhamBenh] ([IDPKB], [TrieuChung], [IDBENH], [IDBacSi]) VALUES (N'PKB2     ', N'Khó Thở, Ho Dai', N'B2       ', N'BS2      ')
+INSERT [dbo].[ChiTietPhieuKhamBenh] ([IDPKB], [TrieuChung], [IDBENH], [IDBacSi]) VALUES (N'PKB3     ', N'Đau Bụng Ở Vùng Rốn, Chán Ăn', N'B3       ', N'BS3      ')
+INSERT [dbo].[ChiTietPhieuKhamBenh] ([IDPKB], [TrieuChung], [IDBENH], [IDBacSi]) VALUES (N'PKB4     ', N'Sưng, Bầm Tím', N'B4       ', N'BS4      ')
+INSERT [dbo].[ChiTietPhieuKhamBenh] ([IDPKB], [TrieuChung], [IDBENH], [IDBacSi]) VALUES (N'PKB5     ', N'Chướng Bụng, Đầy Hơi', N'B5       ', N'BS5      ')
+GO
+INSERT [dbo].[HoSoBenhAn] ([IDHoSo], [HoVaTenBN], [DCHI], [NSINH], [PHONE], [NgayLapHoSo], [NgayHetHanHoSo]) VALUES (N'HS1      ', N'Trần Văn An ', N'Quận 6', CAST(N'2004-02-28T00:00:00.000' AS DateTime), N'0981783235', CAST(N'2024-09-15T00:00:00.000' AS DateTime), CAST(N'2024-09-17T00:00:00.000' AS DateTime))
+INSERT [dbo].[HoSoBenhAn] ([IDHoSo], [HoVaTenBN], [DCHI], [NSINH], [PHONE], [NgayLapHoSo], [NgayHetHanHoSo]) VALUES (N'HS2      ', N'Nguyễn Viết Cường', N'Quận 3', CAST(N'2000-11-22T00:00:00.000' AS DateTime), N'0985823677', CAST(N'2024-09-20T00:00:00.000' AS DateTime), CAST(N'2024-09-23T00:00:00.000' AS DateTime))
+INSERT [dbo].[HoSoBenhAn] ([IDHoSo], [HoVaTenBN], [DCHI], [NSINH], [PHONE], [NgayLapHoSo], [NgayHetHanHoSo]) VALUES (N'HS3      ', N'Hoàng Mạnh Đức', N'Quận 4', CAST(N'2001-05-06T00:00:00.000' AS DateTime), N'0984365478', CAST(N'2024-09-22T00:00:00.000' AS DateTime), CAST(N'2024-09-26T00:00:00.000' AS DateTime))
+INSERT [dbo].[HoSoBenhAn] ([IDHoSo], [HoVaTenBN], [DCHI], [NSINH], [PHONE], [NgayLapHoSo], [NgayHetHanHoSo]) VALUES (N'HS4      ', N'Trương Quang Lâm', N'Quận Phú Nhuận', CAST(N'2003-12-01T00:00:00.000' AS DateTime), N'0987615788', CAST(N'2024-09-27T00:00:00.000' AS DateTime), CAST(N'2024-09-27T00:00:00.000' AS DateTime))
+INSERT [dbo].[HoSoBenhAn] ([IDHoSo], [HoVaTenBN], [DCHI], [NSINH], [PHONE], [NgayLapHoSo], [NgayHetHanHoSo]) VALUES (N'HS5      ', N'Nguyễn Thị Xuân', N'Quận Tân Bình', CAST(N'2004-03-04T00:00:00.000' AS DateTime), N'0983608965', CAST(N'2024-09-27T00:00:00.000' AS DateTime), CAST(N'2024-09-30T00:00:00.000' AS DateTime))
+GO
+INSERT [dbo].[KHOA] ([IDKHoa], [TenKhoa]) VALUES (N'K1       ', N'Khoa Nội')
+INSERT [dbo].[KHOA] ([IDKHoa], [TenKhoa]) VALUES (N'K2       ', N'Khoa Ngoại')
+INSERT [dbo].[KHOA] ([IDKHoa], [TenKhoa]) VALUES (N'K3       ', N'Khoa Sản')
+GO
+INSERT [dbo].[LePhi] ([IDHoaDon], [NgDONG], [SoTienLePhi], [IDHoSo]) VALUES (N'HDLP1    ', CAST(N'2024-09-15T00:00:00.000' AS DateTime), CAST(200.000 AS Decimal(10, 3)), N'HS1      ')
+INSERT [dbo].[LePhi] ([IDHoaDon], [NgDONG], [SoTienLePhi], [IDHoSo]) VALUES (N'HDLP2    ', CAST(N'2024-09-20T00:00:00.000' AS DateTime), CAST(200.000 AS Decimal(10, 3)), N'HS2      ')
+INSERT [dbo].[LePhi] ([IDHoaDon], [NgDONG], [SoTienLePhi], [IDHoSo]) VALUES (N'HDLP3    ', CAST(N'2024-09-22T00:00:00.000' AS DateTime), CAST(150.000 AS Decimal(10, 3)), N'HS3      ')
+INSERT [dbo].[LePhi] ([IDHoaDon], [NgDONG], [SoTienLePhi], [IDHoSo]) VALUES (N'HDLP4    ', CAST(N'2024-09-26T00:00:00.000' AS DateTime), CAST(100.000 AS Decimal(10, 3)), N'HS4      ')
+INSERT [dbo].[LePhi] ([IDHoaDon], [NgDONG], [SoTienLePhi], [IDHoSo]) VALUES (N'HDLP5    ', CAST(N'2024-09-27T00:00:00.000' AS DateTime), CAST(50.000 AS Decimal(10, 3)), N'HS5      ')
+GO
+INSERT [dbo].[PhieuDangKyKhamBenh] ([IDDK], [IDHoSo]) VALUES (N'PDK1     ', N'HS1      ')
+INSERT [dbo].[PhieuDangKyKhamBenh] ([IDDK], [IDHoSo]) VALUES (N'PDK2     ', N'HS2      ')
+INSERT [dbo].[PhieuDangKyKhamBenh] ([IDDK], [IDHoSo]) VALUES (N'PDK3     ', N'HS3      ')
+INSERT [dbo].[PhieuDangKyKhamBenh] ([IDDK], [IDHoSo]) VALUES (N'PDK4     ', N'HS4      ')
+INSERT [dbo].[PhieuDangKyKhamBenh] ([IDDK], [IDHoSo]) VALUES (N'PDK5     ', N'HS5      ')
+GO
+INSERT [dbo].[PhieuKhamBenh] ([IDPKB], [NgayGioKham], [IDHoSo]) VALUES (N'PKB1     ', CAST(N'2024-09-16T08:00:00.000' AS DateTime), N'HS1      ')
+INSERT [dbo].[PhieuKhamBenh] ([IDPKB], [NgayGioKham], [IDHoSo]) VALUES (N'PKB2     ', CAST(N'2024-09-21T08:30:00.000' AS DateTime), N'HS2      ')
+INSERT [dbo].[PhieuKhamBenh] ([IDPKB], [NgayGioKham], [IDHoSo]) VALUES (N'PKB3     ', CAST(N'2024-09-23T10:30:00.000' AS DateTime), N'HS3      ')
+INSERT [dbo].[PhieuKhamBenh] ([IDPKB], [NgayGioKham], [IDHoSo]) VALUES (N'PKB4     ', CAST(N'2024-09-27T14:30:00.000' AS DateTime), N'HS4      ')
+INSERT [dbo].[PhieuKhamBenh] ([IDPKB], [NgayGioKham], [IDHoSo]) VALUES (N'PKB5     ', CAST(N'2024-09-28T15:00:00.000' AS DateTime), N'HS5      ')
+GO
+INSERT [dbo].[PhieuNhapXuatVien] ([IDPHIEU], [NGAYNHAP], [NGAYXUAT], [IDHoSo]) VALUES (N'PNX1     ', CAST(N'2024-09-15T00:00:00.000' AS DateTime), CAST(N'2024-09-17T00:00:00.000' AS DateTime), N'HS1      ')
+INSERT [dbo].[PhieuNhapXuatVien] ([IDPHIEU], [NGAYNHAP], [NGAYXUAT], [IDHoSo]) VALUES (N'PNX2     ', CAST(N'2024-09-20T00:00:00.000' AS DateTime), CAST(N'2024-09-23T00:00:00.000' AS DateTime), N'HS2      ')
+INSERT [dbo].[PhieuNhapXuatVien] ([IDPHIEU], [NGAYNHAP], [NGAYXUAT], [IDHoSo]) VALUES (N'PNX3     ', CAST(N'2024-09-22T00:00:00.000' AS DateTime), CAST(N'2024-09-26T00:00:00.000' AS DateTime), N'HS3      ')
+INSERT [dbo].[PhieuNhapXuatVien] ([IDPHIEU], [NGAYNHAP], [NGAYXUAT], [IDHoSo]) VALUES (N'PNX4     ', CAST(N'2024-09-26T00:00:00.000' AS DateTime), CAST(N'2024-09-29T00:00:00.000' AS DateTime), N'HS4      ')
+INSERT [dbo].[PhieuNhapXuatVien] ([IDPHIEU], [NGAYNHAP], [NGAYXUAT], [IDHoSo]) VALUES (N'PNX5     ', CAST(N'2024-09-27T00:00:00.000' AS DateTime), CAST(N'2024-09-30T00:00:00.000' AS DateTime), N'HS5      ')
+GO
+INSERT [dbo].[PhieuXetNghiem] ([IDXN], [TenXetNghiem], [NgayXetNghiem], [KQUA], [IDHoSo]) VALUES (N'PXN1     ', N'Đo Huyết Áp', CAST(N'2024-09-16T00:00:00.000' AS DateTime), N'Cao Huyết Áp', N'HS1      ')
+INSERT [dbo].[PhieuXetNghiem] ([IDXN], [TenXetNghiem], [NgayXetNghiem], [KQUA], [IDHoSo]) VALUES (N'PXN2     ', N'Xét Nghiệm Máu', CAST(N'2024-09-21T00:00:00.000' AS DateTime), N'BNP Cao, Có Khả Năng Bị Suy Tim', N'HS2      ')
+INSERT [dbo].[PhieuXetNghiem] ([IDXN], [TenXetNghiem], [NgayXetNghiem], [KQUA], [IDHoSo]) VALUES (N'PXN3     ', N'Siêu Âm Bụng', CAST(N'2024-09-23T00:00:00.000' AS DateTime), N'Phát Hiện Viêm Ruột Thừa', N'HS3      ')
+INSERT [dbo].[PhieuXetNghiem] ([IDXN], [TenXetNghiem], [NgayXetNghiem], [KQUA], [IDHoSo]) VALUES (N'PXN4     ', N'Chụp X-quang', CAST(N'2024-09-27T00:00:00.000' AS DateTime), N'Gãy Xương Kín', N'HS4      ')
+INSERT [dbo].[PhieuXetNghiem] ([IDXN], [TenXetNghiem], [NgayXetNghiem], [KQUA], [IDHoSo]) VALUES (N'PXN5     ', N'Chụp CT', CAST(N'2024-09-28T00:00:00.000' AS DateTime), N'Xác Định U Nang', N'HS5      ')
+GO
+INSERT [dbo].[Thuoc] ([IDThuoc], [TenThuoc], [HuongDanSuDung]) VALUES (N'T1       ', N'Thuốc Ức Chế Men Chuyển', N'1-2 viên / ngày')
+INSERT [dbo].[Thuoc] ([IDThuoc], [TenThuoc], [HuongDanSuDung]) VALUES (N'T2       ', N'Thuốc Lợi Tiểu', N'1 viên / ngày')
+INSERT [dbo].[Thuoc] ([IDThuoc], [TenThuoc], [HuongDanSuDung]) VALUES (N'T3       ', N'Kháng Sinh', N'1 viên / ngày')
+INSERT [dbo].[Thuoc] ([IDThuoc], [TenThuoc], [HuongDanSuDung]) VALUES (N'T4       ', N'Giảm Đau', N'2 viên / ngày')
+INSERT [dbo].[Thuoc] ([IDThuoc], [TenThuoc], [HuongDanSuDung]) VALUES (N'T5       ', N'Điều Trị Hormone', N'1 viên / tháng')
+GO
+INSERT [dbo].[ToaThuoc] ([IDToaThuoc], [NgayLapToaThuoc], [IDPKB], [IDBENH], [IDDonThuoc]) VALUES (N'TT1      ', CAST(N'2024-09-16T00:00:00.000' AS DateTime), N'PKB1     ', N'B1       ', N'DT1      ')
+INSERT [dbo].[ToaThuoc] ([IDToaThuoc], [NgayLapToaThuoc], [IDPKB], [IDBENH], [IDDonThuoc]) VALUES (N'TT2      ', CAST(N'2024-09-21T00:00:00.000' AS DateTime), N'PKB2     ', N'B2       ', N'DT2      ')
+INSERT [dbo].[ToaThuoc] ([IDToaThuoc], [NgayLapToaThuoc], [IDPKB], [IDBENH], [IDDonThuoc]) VALUES (N'TT3      ', CAST(N'2024-09-23T00:00:00.000' AS DateTime), N'PKB3     ', N'B3       ', N'DT3      ')
+INSERT [dbo].[ToaThuoc] ([IDToaThuoc], [NgayLapToaThuoc], [IDPKB], [IDBENH], [IDDonThuoc]) VALUES (N'TT4      ', CAST(N'2024-09-27T00:00:00.000' AS DateTime), N'PKB4     ', N'B4       ', N'DT4      ')
+INSERT [dbo].[ToaThuoc] ([IDToaThuoc], [NgayLapToaThuoc], [IDPKB], [IDBENH], [IDDonThuoc]) VALUES (N'TT5      ', CAST(N'2024-09-28T00:00:00.000' AS DateTime), N'PKB5     ', N'B5       ', N'DT5      ')
+GO
+INSERT [dbo].[VienPhi] ([IDVP], [NDONGVP], [TTOANVP], [IDHoSo]) VALUES (N'HDVP1    ', CAST(N'2024-09-17T00:00:00.000' AS DateTime), CAST(900.000 AS Decimal(10, 3)), N'HS1      ')
+INSERT [dbo].[VienPhi] ([IDVP], [NDONGVP], [TTOANVP], [IDHoSo]) VALUES (N'HDVP2    ', CAST(N'2024-09-23T00:00:00.000' AS DateTime), CAST(750.000 AS Decimal(10, 3)), N'HS2      ')
+INSERT [dbo].[VienPhi] ([IDVP], [NDONGVP], [TTOANVP], [IDHoSo]) VALUES (N'HDVP3    ', CAST(N'2024-09-26T00:00:00.000' AS DateTime), CAST(800.000 AS Decimal(10, 3)), N'HS3      ')
+INSERT [dbo].[VienPhi] ([IDVP], [NDONGVP], [TTOANVP], [IDHoSo]) VALUES (N'HDVP4    ', CAST(N'2024-09-29T00:00:00.000' AS DateTime), CAST(500.000 AS Decimal(10, 3)), N'HS4      ')
+INSERT [dbo].[VienPhi] ([IDVP], [NDONGVP], [TTOANVP], [IDHoSo]) VALUES (N'HDVP5    ', CAST(N'2024-09-30T00:00:00.000' AS DateTime), CAST(300.000 AS Decimal(10, 3)), N'HS5      ')
+GO
